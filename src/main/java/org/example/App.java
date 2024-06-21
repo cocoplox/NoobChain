9ae88c6f4f7788c07bfd94a7a36df988fa1c579c
cocoplox <ivanvegasantaella5@gmail.com>
2024-06-22 01:39:03 +0200

    2nd commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,13 @@ package org.example;
 public class App 
 {
     public static void main( String[] args ){
-        System.out.println("Hello World");
+        Block genesisBlock = new Block("Primer bloque","0");
+        System.out.println("Hash del bloque 1 : " + genesisBlock.hash);
+
+        Block secondBlock = new Block("2o bloque", genesisBlock.hash);
+        System.out.println("Hash del bloque 2 : "+ secondBlock.hash);
+
+        Block thirdBlock = new Block("tercer bloque", secondBlock.hash);
+        System.out.println("Hash del bloque 3 : " + thirdBlock.hash);
     }
 }
--- src/main/java/org/example/Block.java
+++ src/main/java/org/example/Block.java
@@ -1,5 +1,26 @@
 package org.example;
 import java.util.Date;
 public class Block {
-    
+    public String hash;
+
+    public String previusHash;
+    private String data; //Datos, que queremos guardar
+    private long timeStamp;
+
+    public Block(String data, String previusHash) {
+
+        this.previusHash = previusHash;
+        this.data = data;
+        this.timeStamp = new Date().getTime();
+        //Importante hacer esto al final de constructor
+        this.hash = CalculateHash();
+    }
+    public String CalculateHash(){
+    String calculatedHash = StringUtil.applySha256(
+            previusHash +
+                    Long.toString(timeStamp) +
+                    data
+                    );
+    return calculatedHash;
+    }
 }
